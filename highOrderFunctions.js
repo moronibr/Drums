6be4fe779c8706d5714baaa53b6af0cbc6cf40ca
Moroni Bamvakiades Ramos <84743417+moronibr@users.noreply.github.com>
2023-01-04 16:56:13 -0300

    Add files via upload

---
+++ highOrderFunctions.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function subtraction(num1, num2) {
+    return num1 - num2
+}
+
+function calculator(num1, num2, operator) {
+    return operator(num1, num2)
+}
+
+console.log(multiply(2, 5))
